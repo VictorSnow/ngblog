ac8ad1d0a289005a5e04f08a380a58d6a021bba4
victor <534436673@qq.com>
2015-08-08 11:36:10 +0000

    remove cache

--- protected/Blog/Controller/PostController.php
+++ protected/Blog/Controller/PostController.php
@@ -1,7 +1,7 @@
 <?php 
     namespace Blog\Controller;
 
-    use \Xend\Controller\AbstractController
+    use \Xend\Controller\AbstractController;
 
     class PostController extends AbstractController{
 
@@ -23,30 +23,18 @@
                 $page = intval($routeinfo['index']);
             }
 
-            $postsCacheKey = 'posts:list:'.$page;
-            $totalCacheKey = 'posts:list:count';
-
-            $posts = $this->getCache()->get($postsCacheKey, function($memc, $key, &$value) use($page,$pagesize){
-                $posts = $this->getCache()->query('select ID,post_title from wp_posts 
-                    where post_status="publish" and post_type in ("post", "revision") 
-                    order by ID desc 
-                    limit '. ($page-1)*$pagesize.','.$pagesize);
-                $value = $posts;
-                $memc->set($key, $value);
-                return false
-            });
-
-            $total = $this->getCache()->get($totalCacheKey,function($memc, $key, &$value){
-                $total = $this->getCache()->query('select count(*) as count from wp_posts
-                    where post_status="publish" and post_type in ("post", "revision")  ');
-                $value = $total;
-                $memc->set($key, $value);
-                return false;
-            });
-
+           
+            $posts = $this->getDb()->query('select ID,post_title from wp_posts 
+                where post_status="publish" and post_type in ("post", "revision") 
+                order by ID desc 
+                limit '. ($page-1)*$pagesize.','.$pagesize);
+           
+	   $total = $this->getDb()->query('select count(*) as count from wp_posts
+                where post_status="publish" and post_type in ("post", "revision")  ');
+           
             return array(
                 'list' => $posts,
-                'total' => $total
+                'total' => $total[0]['count']
             );
         }
     }
